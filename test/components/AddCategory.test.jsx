c78738ffebf557a306761f6b2039638be09d1bfc
[Jonathan] <jonathan13pvc@gmail.com>
2022-06-21 16:45:27 -0500

    second commit

--- test/components/AddCategory.test.jsx
+++ test/components/AddCategory.test.jsx
@@ -1,9 +1,17 @@
-describe('', () => {
+import { fireEvent, render, screen } from "@testing-library/react";
+import { AddCategory } from "../../src/components/AddCategory";
 
-    test('should first', () => { 
+describe(' Pruebas en <AddCategory />', () => {
+
+    test('debe de cambiar el valor en la caja de texto el imput', () => { 
         
+        render( <AddCategory onNewCategory ={ ( ) => {} } /> );
 
+        // screen.debug();
+        const input = screen.getByRole('textbox');
 
+        fireEvent.input( input )
+         screen.debug();
      });
 });
 
